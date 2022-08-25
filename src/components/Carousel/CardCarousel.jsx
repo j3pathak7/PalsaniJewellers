570e0370c9b576150edb7716a24ee9455a85a43b
JP <jyotirmoypathak.37@gmail.com>
2022-08-25 22:03:36 +0530

    ...

--- src/components/Carousel/CardCarousel.jsx
+++ src/components/Carousel/CardCarousel.jsx
@@ -7,27 +7,30 @@ export const CardCarousel = () => {
       <Carousel variant="dark">
         <Carousel.Item>
           <Card className="py-lg-4 px-lg-5 carousel-card">
-            <Card.Title>Maria Sarapavoo</Card.Title>
+            <Card.Title>Bhargaavii Barman</Card.Title>
             <Card.Subtitle className="my-2 mb-4 text-muted">
               Senior Designer
             </Card.Subtitle>
             <p>
-              The point of using Lorem Ipsum is that it has a more-or-less
-              normal distribution of letters, as opposed to using letters Ipsum
-              is that it has a more-or-less normal distribution of letter.
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+              obcaecati error, adipisci at dolore natus placeat deserunt totam
+              sit possimus, rerum repudiandae nemo alias magnam deleniti. Quam
+              temporibus eaque quas iure dolorem fugit reiciendis?
             </p>
           </Card>
         </Carousel.Item>
         <Carousel.Item>
           <Card className="py-lg-4 px-lg-5 carousel-card">
-            <Card.Title>Rose Namajunas</Card.Title>
+            <Card.Title>Mother Teresa</Card.Title>
             <Card.Subtitle className="my-2 mb-4 text-muted">
               Manager Lead
             </Card.Subtitle>
             <p>
-              The point of using Lorem Ipsum is that it has a more-or-less
-              normal distribution of letters, as opposed to using letters Ipsum
-              is that it has a more-or-less normal distribution of letter.
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo
+              tempora nulla enim. Iste magni reiciendis quas, illo similique
+              laudantium ducimus pariatur deserunt facere cupiditate? Facilis
+              officia ipsa labore amet repellendus placeat beatae iusto
+              recusandae?
             </p>
           </Card>
         </Carousel.Item>
